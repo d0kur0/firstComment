a92c68c9c242dc08f348a0ed51e23eb3d487b15f
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-14 13:30:08 +0300

    Удалил логи из класса управления состоянием

--- app/modules/postState.js
+++ app/modules/postState.js
@@ -2,23 +2,19 @@ const log = require('./consoleLogs.js');
 
 module.exports = {
   setPost (post) {
-    log.success(`setPost::${post}`);
     this.post = post;
     this.state = false;
   },
 
   getPost () {
-    log.success(`getPost::${this.post}`);
     return this.post;
   },
 
   setState () {
     this.state = true;
-    log.success(`setState::${this.state}`);
   },
 
   getState () {
-    log.success(`getState::${this.state}`);
     return this.state;
   }
 };
