cc602da200cc67b8272a2c34cb191014381914c8
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-14 11:19:37 +0300

    Добавлен рекурсивный самовызов процесса проверки группы на новые посты, и сама проверка

--- app/modules/process.js
+++ app/modules/process.js
@@ -2,10 +2,24 @@ const puppeteer = require('puppeteer');
 const browserSettings = require('../configs/browserSettings.js');
 const log = require('./consoleLogs.js');
 const auth = require('./auth.js');
+const process = require('../configs/process.js');
+const loop = require('./loop.js');
+const sleep = require('./sleep.js');
+
+global._temp = {};
 
 module.exports = async () => {
   const browser = await puppeteer.launch(browserSettings);
   const page = await browser.newPage();
 
   await auth(page);
+
+  let recursionCall = async () => {
+    await loop(page, process);
+    await sleep(process.delay);
+
+    await recursionCall();
+  };
+
+  await recursionCall();
 };
