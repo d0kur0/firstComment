1bc0b83dae959aa1bb002b3e6bcc6e110694f492
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-14 13:24:02 +0300

    Выводит отладочную информацию о состоянии бота в консоль

--- app/modules/postState.js
+++ app/modules/postState.js
@@ -1,20 +1,24 @@
 const log = require('./consoleLogs.js');
 
-module.exports = () => {
+module.exports = {
   setPost (post) {
+    log.success(`setPost::${post}`);
     this.post = post;
     this.state = false;
-  }
+  },
 
   getPost () {
+    log.success(`getPost::${this.post}`);
     return this.post;
-  }
+  },
 
   setState () {
     this.state = true;
-  }
+    log.success(`setState::${this.state}`);
+  },
 
-  unsetState () {
-    this.state = false;
+  getState () {
+    log.success(`getState::${this.state}`);
+    return this.state;
   }
 };
